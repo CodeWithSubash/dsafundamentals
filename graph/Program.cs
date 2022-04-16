4179233a93233201c1d76a552d0ae7976e657d78
spaudyal <subashpaudyal@gmail.com>
2022-04-15 23:09:51 -0400

    Migrates the first version from other repo

---
+++ graph/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace graph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> data = GetAllLines("tinygraph.txt");
+            int numberOfVertices = Convert.ToInt32(data[0]);
+            int numberOfEdges = Convert.ToInt32(data[1]);
+
+            // Method1
+            //Graph graphObject = new Graph(numberOfVertices);
+            //for (int i = 2; i < data.Count; i++)
+            //{
+            //    string[] item = data[i].Split(' ');
+            //    int u = Convert.ToInt32(item[0]);
+            //    int v = Convert.ToInt32(item[1]);
+            //    graphObject.AddEdge(u, v);
+            //}
+
+            // Method2
+            int[][] edges = new int[numberOfEdges][];
+            for (int i = 2; i < data.Count; i++)
+            {
+                string[] item = data[i].Split(' ');
+                edges[i - 2] = new int[] { Convert.ToInt32(item[0]), Convert.ToInt32(item[1]) };
+            }
+            //Graph graphObject = new Graph(numberOfVertices, edges);
+            Graph graphObject = new Graph(edges);
+
+            // Print all the edges
+            graphObject.DisplayEdges();
+            Console.Read();
+        }
+
+        private static List<string> GetAllLines(string fileName)
+        {
+            var fullPath = Path.Combine(Environment.CurrentDirectory, fileName);
+            return File.ReadAllLines(fullPath).ToList();
+        }
+    }
+}
