8ae3024344f80729577e91a5faa6fa2003e72662
spaudyal <subashpaudyal@gmail.com>
2024-11-06 06:55:57 -0500

    Refactor tree codes to separate project

---
+++ dsa.trees/BinarySearchTree.cs
@@ -0,0 +1,82 @@
+﻿using dsa.core;
+
+namespace dsa.trees
+{
+    internal class BinarySearchTree : BinaryTree
+    {
+        /// <summary>
+        /// Create Binary Search Tree (starting from root as array[0])
+        /// </summary>
+        /// <returns></returns>
+        public BinarySearchTree(int[] arr)
+        {
+            Create(arr);
+        }
+
+        // Asked on ("@IBM Interview!!!");
+        public new void Create(int[] data)
+        {
+            if (data.Length < 1)
+            {
+                root = null;
+            }
+            root = new TreeNode(data[0]);
+            for (int i = 1; i < data.Length; i++)
+            {
+                InsertInBST(root, data[i]);
+            }
+        }
+
+        private static void InsertInBST(TreeNode node, int data)
+        {
+            if (data > node.Value)
+            {
+                if (node.Right != null)
+                {
+                    InsertInBST(node.Right, data);
+                }
+                else
+                {
+                    node.Right = new TreeNode(data);
+                }
+            }
+            else
+            {
+                if (node.Left != null)
+                {
+                    InsertInBST(node.Left, data);
+                }
+                else
+                {
+                    node.Left = new TreeNode(data);
+                }
+            }
+        }
+
+        public new bool Search(int value)
+        {
+            return SearchInternal(root, value);
+        }
+
+        private static bool SearchInternal(TreeNode? tree, int val)
+        {
+            if (tree == null)
+            {
+                return false;
+            }
+            if (tree.Value == val)
+            {
+                return true;
+            }
+            else if (val > tree.Value)
+            {
+                SearchInternal(tree.Right, val);
+            }
+            else
+            {
+                SearchInternal(tree.Left, val);
+            }
+            return false;
+        }
+    }
+}
---
+++ dsa.trees/CompleteBinaryTree.cs
@@ -0,0 +1,32 @@
+﻿using dsa.core;
+
+namespace dsa.trees
+{
+    internal class CompleteBinaryTree : BinaryTree
+    {
+        public CompleteBinaryTree()
+        {
+        }
+
+        public CompleteBinaryTree(int[] data)
+        {
+            Create(data);
+        }
+
+        public new void Create(int[] data)
+        {
+            root = CreateInternal(data);
+        }
+
+        public static TreeNode? CreateInternal(int[] arr, int index = 0, TreeNode? current = null)
+        {
+            if (index < arr.Length)
+            {
+                current = new TreeNode(arr[index]);
+                current.Left = CreateInternal(arr, 2 * index + 1, current.Left);
+                current.Right = CreateInternal(arr, 2 * index + 2, current.Right);
+            }
+            return current;
+        }
+    }
+}
